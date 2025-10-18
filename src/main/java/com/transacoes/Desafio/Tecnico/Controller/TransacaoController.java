56b1c7bbceb6fb49ddc479f4facc422674d433c4
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-10-17 21:47:52 -0300

    Adicionando Endpoint GET - estatistica

--- src/main/java/com/transacoes/Desafio/Tecnico/Controller/TransacaoController.java
+++ src/main/java/com/transacoes/Desafio/Tecnico/Controller/TransacaoController.java
@@ -12,18 +12,24 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/transacao")
+@RequestMapping
 public class TransacaoController {
+
+    private LocalDateTime data;
 
     private List<TransacaoDTO> listTransacao = new ArrayList<>();
 
     private DateTimeFormatter formatter;
 
-    @PostMapping
+    @PostMapping("/transacao")
     public ResponseEntity transacao(@Valid @RequestBody TransacaoDTO trs){
 
         if(trs.valor().compareTo(BigDecimal.ZERO) < 0) return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
@@ -34,9 +40,24 @@ public class TransacaoController {
         return  ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @DeleteMapping
+    @DeleteMapping("/transacao")
     public ResponseEntity deletarTransacoes(){
         listTransacao.clear();
         return ResponseEntity.ok().build();
     };
+
+    @GetMapping("/estatistica")
+    public ResponseEntity transacoes1Minuto(TransacaoDTO dto){
+        List<TransacaoDTO> diferencas = listTransacao.stream()
+                .filter(lt -> ChronoUnit.SECONDS.between(lt.dataHora(), OffsetDateTime.now()) < 61)
+                .collect(Collectors.toList());
+
+        DoubleSummaryStatistics statistics = diferencas.stream()
+                .collect(Collectors.summarizingDouble(
+                        s -> Optional.ofNullable(s.valor())
+                                .orElse(BigDecimal.ZERO)
+                                .doubleValue()));
+
+        return ResponseEntity.ok().body(statistics);
+    }
 }
